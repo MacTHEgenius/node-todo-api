b454e8b7f94d8da860cbfbe59b3c418889c9ca95
Benoit Levesque <genius.blevesque@gmail.com>
2017-04-26 18:10:19 +0000

    Add queries playground

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,27 @@
+const { ObjectID } = require('mongodb');
+const { mongoose } = require('./../server/db/mongoose');
+const { Todo } = require('./../server/models/todo');
+
+var id = '5900de172f5977c913b1f8b6';
+
+if (!ObjectID.isValid(id)) {
+    console.log('Id not valid');
+}
+
+// Todo.find({ _id: id })
+//     .then((todos) => {
+//         console.log('Todos', todos);
+//     });
+    
+// Todo.findOne({ _id: id })
+//     .then((todo) => {
+//         console.log('Todo', todo);
+//     });
+    
+Todo.findById(id)
+    .then((todo) => {
+        if(!todo) {
+            return console.log('Id not found');
+        }
+        console.log('Todo by id', todo);
+    }).catch((e) => console.log(e));
